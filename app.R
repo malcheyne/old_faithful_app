5a1307b5b935b3d6fb5b669e8f9ca02e3ad32d85
malcheyne <92717988+malcheyne@users.noreply.github.com>
2021-12-15 10:40:06 +0000

    Update app.R

--- app.R
+++ app.R
@@ -40,8 +40,10 @@ server <- function(input, output) {
   )
   
   output$distPlot <- renderPlot({
+    # draw the histogram with the specified number of bins
     data() %>% ggplot(aes(x = waiting)) +
-      geom_histogram(bins = input$bins, col = "white", fill = "darkred") +
+      # Modify next line
+      geom_histogram(bins = input$bins, col = "white", fill = "darkgreen") + 
       xlab("Waiting time (mins)") +
       ylab("Number of eruptions") +
       ggtitle("Histogram of eruption waiting times")
